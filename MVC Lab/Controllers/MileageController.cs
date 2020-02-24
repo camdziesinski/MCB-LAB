a0ab608c19b1202e0484d19aac5831e07f7394e7
Cameron Dziesinski <cdziesinski@gmail.com>
2020-02-24 10:18:45 -0500

    MCB lab

---
+++ MVC Lab/Controllers/MileageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Lab.Controllers
+{
+    public class MileageController : Controller
+    {
+        public IActionResult MileageCal()
+        {
+            return View();
+        }
+        public IActionResult Result(int mpg, int gallons)
+        {
+            int mileage = 0;
+            mileage = mpg * gallons;
+            return View(mileage);
+        }
+    }
+}
